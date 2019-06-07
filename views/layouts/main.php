9b679421fef992df8e8b0c7808e2f86ef5cdcddf
Keeller <keellere@rambler.ru>
2019-06-07 15:52:17 +0400

    Added bootstrap

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,15 +1,41 @@
+<?php
+$this->beginPage();
+?>
 <html>
 
 <head>
     <title>video1 school</title>
+    <?php $this->head(); ?>
 
 </head>
 
 <body>
 
-<?= $content?>
+<?php
+$this->beginBody();
+?>
 
+<?php
+\yii\bootstrap\NavBar::begin([
+
+        'brandLabel'=>'MyBrand',
+        'brandUrl'=>Yii::$app->homeUrl,
+            'options'=>[
+                    'class'=>'navbar-default navbar-fixed-top'
+            ]
+]);
+
+\yii\bootstrap\NavBar::end();
+?>
+
+<div class="container" style="margin-top: 70px;">
+<?= $content?>
+</div>
+<?php
+$this->endBody();
+?>
 </body>
 
 
 </html>
+<?php $this->endPage(); ?>
